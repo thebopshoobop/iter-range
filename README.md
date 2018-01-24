e0876d75d3c39faab6beb93a032e2692f828c3a1
Will Timpson <will.timpson@gmail.com>
2018-01-24 00:28:20 -0500

    refactor back into a single class/factory

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ range(4).forEach(() => console.log("#winning"));
 
 This is just one sweet little range library. It implements the same basic API as the [python `range` function](https://docs.python.org/3/library/stdtypes.html?highlight=range#range): `range([start = 0], stop, [step = 1])`. Note that in order to include a step parameter, you must specify a start. The parameters can all be negative, and floating-point numbers work too! If you provide paramaters that describe an impossible or empty range, you will recieve an empty iterator.
 
-In point of fact, this library actually includes _two_ range implementations. The `range` factory function produces instances of the core `Range` object. `Range` objects are iterable and have lazily evaluated versions of the `forEach`, `map`, and `reduce` methods and a (constant time) `length` property. The `derange` function builds instances of the `Derange` object, which extend `Range` and include and a whole bunch more of the `Array.prototype` methods. I have divided them so as to keep the core library small. I named the extended version `Derange` to reflect my mental state for feeling the need to build those additional features.
+This library exports a singe function. The `range` factory function produces instances of the core `Range` object. `Range` objects are iterable and have lazily evaluated versions of many `Array.prototype` methods including `forEach`, `map`, and `reduce` methods, along with a (constant time) `length` property.
 
-The key differentiator between this library and the other JavaScript range libraries that I have seen is that it does not create and populate arrays (other than `map`). Instead, I provide you with factory functions that build iterator objects. These objects include implementations of several Array.prototype methods that match their Array counterparts nearly exactly. If you really want an array, you can always use `Array.from(range(2, 12))` or spread the iterator `[...range(5)]`. Note that (with the exception of `map`, `reduce`, `reduceRight`, and `filter`), these are all constant-space methods. They use the iterator backend and don't create any additional arrays or objects. Furthermore, they are written to break early if possible, so a `some` call that matches on the first item stops there and returns.
+The key differentiator between this library and the other JavaScript range libraries that I have seen is that it does not create and populate arrays (other than `map`). Instead, I provide you with factory functions that build iterator objects. These objects include implementations of several `Array.prototype` methods that match their Array counterparts nearly exactly. If you really want an array, you can always use `Array.from(range(2, 12))` or spread the iterator `[...range(5)]`. Note that (with the exception of `map`, `reduce`, `reduceRight`, and `filter`), these are all constant-space methods. They use the iterator backend and don't create any additional arrays or objects. Furthermore, they are written to break early if possible, so a `some` call that matches on the first item stops there and returns.
 
 ## Range
 
@@ -45,24 +45,11 @@ console.log(...r); //=> 0 1 2
 console.log(range(0, 10, 3).length); //=> 4
 ```
 
-Additionally, `Range` objects have three of the standard Array functional iteration methods:
+Additionally, `Range` objects have a whole possee of the standard `Array.prototype` methods:
 
 * `forEach(callback, [thisArg])`
 * `map(callback, [thisArg])`
 * `reduce(callback, [accumulator])`
-
-```js
-const { range } = require("iter-range");
-console.log(range(3).map(i => i ** 3)); //=> [0, 8, 27]
-console.log(range(3, 6).map((i, index) => [i, index])); //=> [[3, 0], [4, 1], [5, 2]]
-```
-
-These methods are all written to consume the iterator rather than creating an array to iterate over (`map`, of course, builds an array to return). If you need a refresher, you should check the [MDN Array documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array). The important bit is that the callbacks (except for `reduce`, which getss the accumulator first) will be called with three parameters: `currentValue`, `index`, and `Range`. The only difference is the third one. Since these methods don't construct an array to iterate over, there is no array to pass. Instead they pass the `Range` object as the third parameter. I hope that's helpful.
-
-## Derange
-
-So `Range`s are pretty cool... but I need moar! Fortunately, I've got you covered. The `derange` factory will happily build you instances of the feature-ful `Derange`. These puppies extend `Range` so as to include all of their goodness, but they also have a whole additional posse of fun methods:
-
 * `reduceRight(callback, [accumulator])`
 * `every(callback, [thisArg])`
 * `some(callback, [thisArg])`
@@ -74,14 +61,19 @@ So `Range`s are pretty cool... but I need moar! Fortunately, I've got you covere
 * `lastIndexOf(searchElement, [fromIndex])`
 * `reverse()`
 
-These methods' callbacks all get those same three parameters as above (except `reduceRight`, which is just like `reduce`). Also note that negative values _are_ supported for `fromIndex`; they are treated as indexes from the _right_ side of the range). One other difference is that reverse does _not_ mutate the `Derange` it is called on, but instead returns a new instance.
-
 ```js
-const { derange } = require("iter-range");
-console.log(...derange(5).reverse()); //=> 4 3 2 1 0
-console.log(derange(8).filter(i => i % 2 === 0)); //=> [0, 2, 4, 6]
+const { range } = require("iter-range");
+console.log(range(3).map(i => i ** 3)); //=> [0, 8, 27]
+console.log(range(3, 6).map((i, index) => [i, index])); //=> [[3, 0], [4, 1], [5, 2]]
+console.log(...range(5).reverse()); //=> 4 3 2 1 0
+console.log(range(8).filter(i => i % 2 === 0)); //=> [0, 2, 4, 6]
+console.log(range(2.5, -2.75, -0.25).includes(1)); //=> true
 ```
+
+These methods are all written to consume the iterator rather than creating an array to iterate over (`map`, of course, builds an array to return). If you need a refresher, you should check the [MDN Array documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array). The important bit is that the callbacks (except for `reduce` and `reduceRight`, which get the accumulator first) will be called with three parameters: `currentValue`, `index`, and `Range`. The only difference is the third one. Since these methods don't construct an array to iterate over, there is no array to pass. Instead they pass the `Range` object as the third parameter. I hope that's helpful.
+
+Also note that negative values _are_ supported for `fromIndex`; they are treated as indexes from the _right_ side of the range). One other difference is that reverse does _not_ mutate the `Derange` it is called on, but instead returns a new instance.
 
 ## Testing
 
-The `Range` and `Derange` objects are thoroughly tested to match their Array.prototype counterparts (except as noted). Please let me know if I've missed or wrongly implemented anything. You can run the tests by cloning the repository, and running [Jasmine](https://jasmine.github.io/). If you don't have it installed globally, you can simply `$ npm i` and then `$ npm test` will run the suite. [Istanbul](https://istanbul.js.org/) is used to ensure complete test coverage. You can run the tests and generate a coverage report with `$ npm run coverage`. Likewise, you can lint the code with `$ npm run lint`.
+The `Range` object methods are thoroughly tested to match their Array.prototype counterparts (except as noted). Please let me know if I've missed or wrongly implemented anything. You can run the tests by cloning the repository, and running [Jasmine](https://jasmine.github.io/). If you don't have it installed globally, you can simply `$ npm i` and then `$ npm test` will run the suite. [Istanbul](https://istanbul.js.org/) is used to ensure complete test coverage. You can run the tests and generate a coverage report with `$ npm run coverage`. Likewise, you can lint the code with `$ npm run lint`.
--- spec/rangeSpec.js
+++ spec/rangeSpec.js
@@ -1,4 +1,4 @@
-const { range } = require("../lib");
+const range = require("../lib/range");
 
 describe("The range function", () => {
   describe("when considering the iterator properties,", () => {
@@ -18,6 +18,7 @@ describe("The range function", () => {
 
       expect([...r]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
       expect([...r]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      expect(r.map(i => i ** 2)).toEqual([0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
     });
 
     it("should treat a single parameter as a max and start from 0", () => {
@@ -45,7 +46,7 @@ describe("The range function", () => {
         expect([...range(10, 1)]).toEqual([]);
       });
 
-      it("should handle a smaler start than end and negative increment", () => {
+      it("should handle a decreasing step with increasing start, stop", () => {
         expect([...range(1, 10, -5)]).toEqual([]);
       });
 
@@ -185,6 +186,433 @@ describe("The range function", () => {
         it("should throw a TypeError if there is no accumulator", () => {
           try {
             range(3, 2).reduce(callback);
+          } catch (error) {
+            expect(error.name).toEqual("TypeError");
+            expect(callback).not.toHaveBeenCalled();
+            callback("caught");
+          } finally {
+            expect(callback).toHaveBeenCalledWith("caught");
+          }
+        });
+      });
+    });
+
+    describe("when using the every method,", () => {
+      it("should check every element, if necessary", () => {
+        range(5).every(spy.every);
+
+        expect(spy.every).toHaveBeenCalledTimes(5);
+      });
+
+      it("should return true if every element does", () => {
+        expect(range(10).every(spy.every)).toEqual(true);
+      });
+
+      it("should not check every element if not necessary", () => {
+        range(5).every(spy.none);
+
+        expect(spy.none).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return false if any item does", () => {
+        expect(range(9).every(spy.none)).toEqual(false);
+      });
+
+      it("should pass the index and the range to the every callback", () => {
+        const r = range(5, 10);
+        r.every(spy.every);
+
+        expect(spy.every).toHaveBeenCalledWith(7, 2, r);
+      });
+
+      it("should default `this` to the range in the every callback", () => {
+        const r = range(22, 33, 0.25);
+
+        r.every(function() {
+          expect(this).toBe(r);
+        });
+      });
+
+      it("should assign `this` in the every callback when given one", () => {
+        range(6, 10).every(function() {
+          expect(this).toBe(spy.every);
+        }, spy.every);
+      });
+
+      it("should check every element", () => {
+        expect(range(0, 10, 2).every(i => i % 2 === 0)).toEqual(true);
+        expect(range(0, 10).every(i => i % 2 === 0)).toEqual(false);
+      });
+
+      it("should reset the iterator if not every element matches", () => {
+        const r = range(5);
+
+        expect(r.every(i => i >= 2)).toBe(false);
+        expect(r.every(i => i >= 2)).toBe(false);
+        expect(r.every(i => i >= 2)).toBe(false);
+      });
+    });
+
+    describe("when using the some method,", () => {
+      it("should call the some callback for every element if necessary", () => {
+        range(5).some(spy.none);
+
+        expect(spy.none).toHaveBeenCalledTimes(5);
+      });
+
+      it("should return true if any element does", () => {
+        expect(range(10).some(spy.every)).toEqual(true);
+      });
+
+      it("should call the some callback as few times as possible", () => {
+        range(5).some(spy.every);
+
+        expect(spy.every).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return false if no item does", () => {
+        expect(range(9).some(spy.none)).toEqual(false);
+      });
+
+      it("should pass the index and the range to the some callback", () => {
+        const r = range(5, 10);
+        r.some(spy.none);
+
+        expect(spy.none).toHaveBeenCalledWith(9, 4, r);
+      });
+
+      it("should default `this` to the range in the some callback", () => {
+        const r = range(22, 33, 0.25);
+
+        r.some(function() {
+          expect(this).toBe(r);
+        });
+      });
+
+      it("should assign `this` in the some callback when given one", () => {
+        range(6, 10).some(function() {
+          expect(this).toBe(spy.every);
+        }, spy.every);
+      });
+
+      it("should check some elements", () => {
+        expect(range(0, 10, 2).some(i => i % 2 === 0)).toEqual(true);
+        expect(range(1, 11, 2).some(i => i % 2 === 0)).toEqual(false);
+      });
+
+      it("should reset the iterator if some element matches", () => {
+        const r = range(1, 4);
+
+        expect(r.some(i => i % 2 === 0)).toBe(true);
+        expect(r.some(i => i % 2 === 0)).toBe(true);
+      });
+    });
+
+    describe("when using the filter method,", () => {
+      it("should call the filter callback for every element", () => {
+        range(5).filter(spy.none);
+
+        expect(spy.none).toHaveBeenCalledTimes(5);
+      });
+
+      it("should return elements that are truthy", () => {
+        expect(range(9).filter(spy.every)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+      });
+
+      it("should not return falsy elements", () => {
+        expect(range(9).filter(spy.none)).toEqual([]);
+      });
+
+      it("should pass the index and the range to the filter callback", () => {
+        const r = range(2, 12, 2);
+        r.filter(spy.none);
+
+        expect(spy.none).toHaveBeenCalledWith(8, 3, r);
+      });
+
+      it("should default `this` to the range in the filter callback", () => {
+        const r = range(12, 5 - 2);
+
+        r.filter(function() {
+          expect(this).toBe(r);
+        });
+      });
+
+      it("should assign `this` in the filter callback when given one", () => {
+        range(6, 10).filter(function() {
+          expect(this).toBe(spy.every);
+        }, spy.every);
+      });
+
+      it("should filter elements", () => {
+        expect(range(0, 7).filter(i => i % 2 === 0)).toEqual([0, 2, 4, 6]);
+        expect(range(1, 11, 2).filter(i => i % 2 === 0)).toEqual([]);
+      });
+    });
+
+    describe("when using the find method,", () => {
+      it("should check every element if necessary", () => {
+        range(5).find(spy.none);
+
+        expect(spy.none).toHaveBeenCalledTimes(5);
+      });
+
+      it("should check the minimum elements", () => {
+        range(5).find(spy.every);
+
+        expect(spy.every).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return the first truthy element", () => {
+        expect(range(9).find(spy.every)).toEqual(0);
+      });
+
+      it("should return undefined if all elements are falsy", () => {
+        expect(range(9).find(spy.none)).toEqual(undefined);
+      });
+
+      it("should pass the index and the range to the find callback", () => {
+        const r = range(2, 12, 2);
+        r.find(spy.none);
+
+        expect(spy.none).toHaveBeenCalledWith(8, 3, r);
+      });
+
+      it("should default `this` to the range in the find callback", () => {
+        const r = range(12, 5 - 2);
+
+        r.find(function() {
+          expect(this).toBe(r);
+        });
+      });
+
+      it("should assign `this` in the find callback when given one", () => {
+        range(6, 10).find(function() {
+          expect(this).toBe(spy.every);
+        }, spy.every);
+      });
+
+      it("should find elements", () => {
+        expect(range(1, 7).find(i => i % 4 === 0)).toEqual(4);
+        expect(range(1, 11, 2).find(i => i % 2 === 0)).toEqual(undefined);
+      });
+
+      it("should reset the iterator if a match is found", () => {
+        const r = range(5);
+
+        expect(r.find(i => i === 3)).toBe(3);
+        expect(r.find(i => i === 3)).toBe(3);
+      });
+    });
+
+    describe("when using the findIndex method,", () => {
+      it("should call the findIndex callback as many times as needed", () => {
+        range(5).findIndex(spy.none);
+
+        expect(spy.none).toHaveBeenCalledTimes(5);
+      });
+
+      it("should call the findIndex callback as few times as needed", () => {
+        range(5).findIndex(spy.every);
+
+        expect(spy.every).toHaveBeenCalledTimes(1);
+      });
+
+      it("should return the index of the first truthy element", () => {
+        expect(range(9).findIndex(spy.every)).toEqual(0);
+      });
+
+      it("should return -1 if all elements are falsy", () => {
+        expect(range(9).findIndex(spy.none)).toEqual(-1);
+      });
+
+      it("should pass the index and range to the findIndex callback", () => {
+        const r = range(2, 12, 2);
+        r.findIndex(spy.none);
+
+        expect(spy.none).toHaveBeenCalledWith(8, 3, r);
+      });
+
+      it("should default `this` to the range in the findIndex callback", () => {
+        const r = range(12, 5 - 2);
+
+        r.findIndex(function() {
+          expect(this).toBe(r);
+        });
+      });
+
+      it("should assign a given `this` in the findIndex callback", () => {
+        range(6, 10).findIndex(function() {
+          expect(this).toBe(spy.every);
+        }, spy.every);
+      });
+
+      it("should find the index of elements", () => {
+        expect(range(1, 7).findIndex(i => i % 4 === 0)).toEqual(3);
+        expect(range(1, 11, 2).findIndex(i => i % 2 === 0)).toEqual(-1);
+      });
+
+      it("should reset the iterator if a finIndex callback matches early", () => {
+        const r = range(10, 0, -2);
+
+        expect(r.findIndex(i => i === 6)).toEqual(2);
+        expect(r.findIndex(i => i === 6)).toEqual(2);
+      });
+    });
+
+    describe("when using the indexOf method,", () => {
+      it("should return the index of included numbers", () => {
+        expect(range(0, 10, 2).indexOf(6)).toEqual(3);
+        expect(range(10, 0, -2).indexOf(6)).toEqual(2);
+        expect(range(10).indexOf(0)).toEqual(0);
+      });
+
+      it("should return -1 for non-included numbers", () => {
+        expect(range(0, 10, 2).indexOf(5)).toEqual(-1);
+        expect(range(5).indexOf(10)).toEqual(-1);
+        expect(range(5, 10).indexOf(2)).toEqual(-1);
+      });
+
+      it("should accept and start from a fromIndex parameter", () => {
+        expect(range(5).indexOf(4, 2)).toEqual(4);
+        expect(range(10).indexOf(4, 5)).toEqual(-1);
+      });
+
+      it("should treat a negative fromIndex as an offset from the end", () => {
+        expect(range(10).indexOf(8, -4)).toEqual(8);
+        expect(range(10).indexOf(3, -3)).toEqual(-1);
+      });
+
+      it("should reset the iterator if an index is found", () => {
+        const r = range(6);
+
+        expect(r.indexOf(3)).toEqual(3);
+        expect(r.indexOf(3)).toEqual(3);
+      });
+    });
+
+    describe("when using the includes method,", () => {
+      it("should return true for included numbers", () => {
+        expect(range(0, 10, 2).includes(6)).toEqual(true);
+        expect(range(10, 0, -2).includes(6)).toEqual(true);
+        expect(range(10).includes(0)).toEqual(true);
+      });
+
+      it("should return false for non-included numbers", () => {
+        expect(range(0, 10, 2).includes(5)).toEqual(false);
+      });
+
+      it("should accept a fromIndex parameter to begin inclusion check", () => {
+        expect(range(5).includes(4, 2)).toEqual(true);
+        expect(range(10).includes(4, 5)).toEqual(false);
+      });
+
+      it("should use a negative fromIndex as an offset from the end", () => {
+        expect(range(10).includes(8, -4)).toEqual(true);
+        expect(range(10).includes(3, -3)).toEqual(false);
+      });
+
+      it("should reset the iterator if there is an includes match", () => {
+        const r = range(10, 20);
+
+        expect(r.includes(13)).toEqual(true);
+        expect(r.includes(13)).toEqual(true);
+      });
+    });
+
+    describe("when using the reverse method,", () => {
+      it("should not reverse the original range", () => {
+        const r = range(8);
+        r.reverse();
+
+        expect([...r]).toEqual([0, 1, 2, 3, 4, 5, 6, 7]);
+      });
+
+      it("should return a new range", () => {
+        const r = range(5);
+
+        expect(r.reverse()).not.toBe(r);
+      });
+
+      it("should return a reversed range", () => {
+        expect([...range(6).reverse()]).toEqual([5, 4, 3, 2, 1, 0]);
+        expect([...range(10, 0, -2).reverse()]).toEqual([2, 4, 6, 8, 10]);
+      });
+    });
+
+    describe("when using the lastIndexOf method,", () => {
+      it("shoud return the last index of included members", () => {
+        expect(range(12, 24).lastIndexOf(18)).toEqual(6);
+      });
+
+      it("should return -1 for non-included items", () => {
+        expect(range(12, 22).lastIndexOf(4)).toEqual(-1);
+      });
+
+      it("should accpet a fromIndex parameter to reverse search from", () => {
+        expect(range(5, 10).lastIndexOf(6, 4)).toEqual(1);
+        expect(range(5, 10).lastIndexOf(9, 3)).toEqual(-1);
+      });
+
+      it("should handle too-large fromIndex params", () => {
+        expect(range(5, 10).lastIndexOf(7, 10)).toEqual(2);
+      });
+
+      it("should handle a negative fromIndex", () => {
+        expect(range(5, 10).lastIndexOf(6, -2)).toEqual(1);
+        expect(range(5, 10).lastIndexOf(9, -3)).toEqual(-1);
+        expect(range(10).lastIndexOf(4, -12)).toEqual(-1);
+      });
+
+      it("should reset the iterator when finding the last index", () => {
+        const r = range(10);
+
+        expect(r.lastIndexOf(3)).toEqual(3);
+        expect(r.lastIndexOf(3)).toEqual(3);
+      });
+    });
+
+    describe("when using the reduceRight method", () => {
+      it("should reduce the callback over each element in reverse", () => {
+        range(5).reduceRight(callback, 0);
+
+        expect(callback).toHaveBeenCalledTimes(5);
+      });
+
+      it("should pass the index and range to the reduceRight callback", () => {
+        const r = range(5, 10);
+        r.reduceRight(callback, 0);
+
+        expect(callback).toHaveBeenCalledWith(undefined, 8, 3, r);
+      });
+
+      it("should reduce the range", () => {
+        const r = range(5).reduceRight(
+          (n, i) => ({ ...n, [i]: i % 2 === 0 }),
+          {}
+        );
+
+        expect(r).toEqual({ 0: true, 1: false, 2: true, 3: false, 4: true });
+        expect(range(5).reduceRight((sum, i) => sum + i, 0)).toEqual(10);
+      });
+
+      it("should use the last item if no accumulator is given", () => {
+        const r = range(5);
+        r.reduceRight(callback);
+
+        expect(callback).toHaveBeenCalledWith(4, 3, 3, r);
+        expect(range(5).reduceRight((sum, i) => sum + i)).toEqual(10);
+      });
+
+      describe("when given an empty range to reduce,", () => {
+        it("should return the accumulator and not call the callback", () => {
+          expect(range(0, 10, -2).reduceRight(callback, spy)).toBe(spy);
+          expect(callback).not.toHaveBeenCalled();
+        });
+
+        it("should throw a TypeError if there is no accumulator param", () => {
+          try {
+            range(3, 2).reduceRight(callback);
           } catch (error) {
             expect(error.name).toEqual("TypeError");
             expect(callback).not.toHaveBeenCalled();
